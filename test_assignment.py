50bbc8d62338b99d71e2daeb87318fb8960d25d5
samir <khatrisamir10@gmail.com>
2025-02-07 08:35:25 +0545

    control-structures assignment submitted

--- test_assignment.py
+++ test_assignment.py
@@ -1,7 +1,7 @@
 import io
 import sys
 import pytest
-# import assignment  # Assumes the student's solution is in assignment.py
+#import assignment  # Assumes the student's solution is in assignment.py
 
 import nbconvert
 import io
@@ -30,7 +30,7 @@ def import_notebook_module(notebook_path):
     return module
 
 # Then use in tests
-assignment = import_notebook_module('assignment.ipynb')
+assignment = import_notebook_module('starter_code.ipynb')
 
 def test_while_loop_even_numbers(capsys):
     """Test the while loop for printing even numbers up to 16"""
